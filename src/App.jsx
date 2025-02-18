0a1d7d435fde0b470851612c652a1eb7487712f4
flo-jee <tuberose1621@gmail.com>
2025-02-19 05:31:44 +0900

    기능 추가: 현재 시간 표시, 타이머, 스톱워치 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
-import { useEffect, useRef, useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import "./App.css";
+
 // todo 생성
 function App() {
   const [todo, setTodo] = useState([
@@ -11,12 +12,122 @@ function App() {
 
   return (
     <>
+      <Clock />
+      <StopWatch />
+      <Timer />
       <TodoInput setTodo={setTodo} />
       <TodoList todo={todo} setTodo={setTodo} />
       <h1>TODO LIST</h1>
     </>
   );
 }
+// 현재 시각 구현
+const Clock = () => {
+  const [time, setTime] = useState(new Date());
+  useEffect(() => {
+    setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+  }, []);
+
+  return <div>{time.toLocaleTimeString()}</div>;
+};
+
+// 스톱워치 초기화
+const formatTime = (seconds) => {
+  const timeString = `${String(Math.floor(seconds / 3600)).padStart(2, "0")}
+  : ${String(Math.floor((seconds % 3600) / 60)).padStart(2, "0")}
+  : ${String(seconds % 60).padStart(2, "0")}`;
+  return timeString;
+};
+// 스톱워치
+const StopWatch = () => {
+  const [time, setTime] = useState(0);
+  const [isOn, setIsOn] = useState(false);
+  const timerRef = useRef(null);
+  useEffect(() => {
+    if (isOn === true) {
+      const timerId = setInterval(() => {
+        setTime((prev) => prev + 1);
+        timerRef.current = timerId;
+      }, 1000);
+    } else {
+      clearInterval(timerRef.current);
+    }
+  }, [isOn]);
+
+  return (
+    <div>
+      {formatTime(time)}
+      <button onClick={() => setIsOn((prev) => !prev)}>
+        {isOn ? "끄기" : "켜기"}
+      </button>
+      <button
+        onClick={() => {
+          setTime(0);
+          setIsOn(false);
+        }}
+      >
+        리셋
+      </button>
+    </div>
+  );
+};
+
+// 타이머
+const Timer = () => {
+  const [startTime, setStartTime] = useState(0);
+  const [isOn, setIsOn] = useState(false);
+  const [time, setTime] = useState(0);
+  const timerRef = useRef(null);
+
+  useEffect(() => {
+    if (isOn && time > 0) {
+      const timerId = setInterval(() => {
+        setTime((prev) => prev - 1);
+      }, 1000);
+      timerRef.current = timerId;
+    } else if (!isOn || time == 0) {
+      clearInterval(timerRef.current);
+    }
+    return () => clearInterval(timerRef.current);
+  }, [isOn, time]);
+
+  return (
+    <div>
+      <div>
+        {time ? formatTime(time) : formatTime(startTime)}
+        <button
+          onClick={() => {
+            setIsOn(true);
+            setTime(time ? time : startTime);
+            setStartTime(0);
+          }}
+        >
+          시작
+        </button>
+        <button onClick={() => setIsOn(false)}>멈춤</button>
+        <button
+          onClick={() => {
+            setTime(0);
+            setIsOn(false);
+          }}
+        >
+          리셋
+        </button>
+      </div>
+
+      <input
+        type="range"
+        value={startTime}
+        min="0"
+        max="3600"
+        step="30"
+        onChange={(event) => setStartTime(event.target.value)}
+      />
+    </div>
+  );
+};
 
 // todo 생성
 const TodoInput = ({ setTodo }) => {
@@ -36,7 +147,6 @@ const TodoInput = ({ setTodo }) => {
     </>
   );
 };
-
 // todo 수정
 const TodoList = ({ todo, setTodo, currentTodo, setCurrentTodo }) => {
   return (
@@ -47,7 +157,6 @@ const TodoList = ({ todo, setTodo, currentTodo, setCurrentTodo }) => {
     </ul>
   );
 };
-
 // todo 삭제
 const Todo = ({ todo, setTodo, currentTodo, setCurrentTodo }) => {
   console.log(todo);
